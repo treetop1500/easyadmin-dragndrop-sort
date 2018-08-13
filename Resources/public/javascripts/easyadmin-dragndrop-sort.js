d37537cfe7d8a61ad35f419b8c68b9f0c3995025
rwade <rwade@grayloon.com>
2018-08-13 17:02:01 -0500

    cleans up javascript

--- Resources/public/javascripts/easyadmin-dragndrop-sort.js
+++ Resources/public/javascripts/easyadmin-dragndrop-sort.js
@@ -1,4 +1,4 @@
-var easyadminDragndropSort =
+let easyadminDragndropSort =
     {
         initDraggableEntityRows: function() {
 
@@ -6,31 +6,31 @@ var easyadminDragndropSort =
                 return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
             }
 
-            var table;
-            var tbody;
+            let table;
+            let tbody;
 
-            var body = document.getElementsbyTagName("body")[0];
+            let body = document.getElementsByTagName("body")[0];
             if(hasClass(body, "easyadmin-list-Page")){
 
-                var content = body.getElementById("main")
+                let content = body.getElementById("main")
                 if(hasClass(content, "content")){
 
                     table = content.getElementsByClassName("table")[0];
                     tbody = content.getElementsByClassName("table")[0];
 
-                    var tableRows = table.getElementsByTagName("tr");
+                    let tableRows = table.getElementsByTagName("tr");
 
                     for (row in tableRows) {
                         row.setAttribute('draggable', true);
                     }
-                };
-            };
+                }
+            }
 
-            var dragSrcEl = null; // the object being drug
-            var startPosition = null; // the index of the row element (0 through whatever)
-            var endPosition = null; // the index of the row element being dropped on (0 through whatever)
-            var parent; // the parent element of the dragged item
-            var entityId; // the id (key) of the entity
+            let dragSrcEl = null; // the object being drug
+            let startPosition = null; // the index of the row element (0 through whatever)
+            let endPosition = null; // the index of the row element being dropped on (0 through whatever)
+            let parent; // the parent element of the dragged item
+            let entityId; // the id (key) of the entity
 
             function handleDragStart(e) {
                 dragSrcEl = e.currentTarget;
@@ -70,7 +70,7 @@ var easyadminDragndropSort =
                 }
 
                 // Don't do anything if dropping the same column we're dragging.
-                if (dragSrcEl != this) {
+                if (dragSrcEl !== this) {
                     endPosition = Array.prototype.indexOf.call(parent.children, this);
                     console.log("end: "+endPosition);
                     // Set the source column's HTML to the HTML of the column we dropped on.
@@ -78,13 +78,13 @@ var easyadminDragndropSort =
                     this.innerHTML = e.dataTransfer.getData('text/html');
 
                     // do the ajax call to update the database
-                    var xhr = new XMLHttpRequest();
+                    let xhr = new XMLHttpRequest();
                     xhr.open('GET', (location.pathname+location.search).substr(1));
                     xhr.onload = function() {
                         if (xhr.status === 200) {
-                            var response = xhr.responseText;
-                            var newBody = response.getElementsbyTagName("body")[0];
-                            var newTbody = newBody.getElementsbyTagName("tbody")[0];
+                            let response = xhr.responseText;
+                            let newBody = response.getElementsByTagName("body")[0];
+                            let newTbody = newBody.getElementsByTagName("tbody")[0];
                             tbody.parentNode.replaceChild(newTbody, tbody);
                             easyadminDragndropSort.initDraggableEntityRows();
                         }
@@ -106,7 +106,7 @@ var easyadminDragndropSort =
                     row.classList.remove('over');
                 });
             }
-            var rows = document.querySelectorAll('table.sortable > tbody tr');
+            let rows = document.querySelectorAll('table.sortable > tbody tr');
             [].forEach.call(rows, function(row) {
                 row.addEventListener('dragstart', handleDragStart, false);
                 row.addEventListener('dragenter', handleDragEnter, false);
